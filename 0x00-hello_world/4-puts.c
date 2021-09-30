3af814be9ec5d299cf17b0ff7d9cc2c24953e9c5
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-09-30 06:36:39 -0400

    first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+/**
+ * main - C program that prints a message
+ * Return: return int
+ *
+ */
 int main(void)
-
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
-
+	puts( "\"Programming is like building a multilingual puzzle");
+	return(0);
 }
-
