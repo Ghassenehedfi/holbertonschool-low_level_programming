d8cda0269528d67b12e28667be10352291ab492b
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-09-30 08:08:36 -0400

    first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,6 @@ int main()
 	printf("%zu\n", sizeof(int));
 	printf("%zu\n", sizeof(float));
 	printf("%zu bytes\n", sizeof(long));
-	printf("Size of long long: %zu bytes\n", sizeof(long long));
+	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
 	return(0);
 }
