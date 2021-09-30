aef49da054333cb2aee00527cc28071dacf28218
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-09-30 07:32:19 -0400

    first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,14 +6,14 @@
 int main() {
     int intType;
     float floatType;
-    double doubleType;
+    long int longinType;
+    long long int longlongintType;
     char charType;
-
-    // sizeof evaluates the size of a variable
-    printf("Size of int: %zu bytes\n", sizeof(intType));
-    printf("Size of float: %zu bytes\n", sizeof(floatType));
-    printf("Size of double: %zu bytes\n", sizeof(doubleType));
-    printf("Size of char: %zu byte\n", sizeof(charType));
-    
+	 // sizeof evaluates the size of a variable
+	 printf("Size of int: %zu bytes\n", sizeof(intType));
+   	 printf("Size of float: %zu bytes\n", sizeof(floatType));
+    	 printf("sizeof of long: %zu\n", sizeof(longintType));
+   	 printf("Size of char: %zu byte\n", sizeof(charType));
+         printf("size of long long: %zu byte\n", sizeof(longlongintType));
     return 0;
 }
