ae4a0666d589ab932cc73c61ab45ebc39c3427ba
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-09-30 08:02:04 -0400

    first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,12 @@
 int void main()
 {
 	clrscr();
-	printf(“TypettttSize (bytes)”);
-	printf(“nCharacterttt    %d”,sizeof(char));
-	printf(“nIntegertttt    %d”,sizeof(int));
-	printf(“nLong intttt    %d”,sizeof(long int));
-	printf(“nFloattttt    %d”,sizeof(float));
-	printf(“nDoubletttt    %d”,sizeof(double));
-	printf(“nLong doublettt    %d”,sizeof(long double));
+	printf("TypettttSize (bytes));
+	printf("nCharacterttt    %d”,sizeof(char));
+	printf("nIntegertttt    %d”,sizeof(int));
+	printf("nLong intttt    %d”,sizeof(long int));
+	printf("nFloattttt    %d”,sizeof(float));
+	printf("nDoubletttt    %d”,sizeof(double));
+	printf("nLong doublettt    %d”,sizeof(long double));
 	getch();
 }
