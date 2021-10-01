8bf76359ea3be31d29d26a5528884b1d888c9d7a
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-10-01 22:38:28 +0100

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - print comb
+ * Return: 0
+ */
+int main(void)
+
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+if (i !=9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
