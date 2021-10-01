4684a096999c2a590bdd18ae46a84038a436c414
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-10-01 09:49:08 -0400

    first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 #include <locale.h>
 #include <unistd.h>
 /**
- * main -  assign a random number 
+ * main - assign a random number 
  * Return: 0
  */
 int main(void)
@@ -12,12 +12,12 @@ int main(void)
 int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;    
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-        printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	else if (n < 0)
-        printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	else if (n == 0)
-        printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	return 0;
 }
