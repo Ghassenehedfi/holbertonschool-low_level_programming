382e81f04eae3c6d00c01d27ffd8bf871539c51d
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-10-01 22:04:23 +0100

    print alpphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,11 +4,12 @@
  * Return: 0
  */
 int main(void)
+  
 {
   char c;
   for (c = 'a'; c <= 'z'; c++)
     {
-      if (c == 'q'; c == 'e')
+      if (c == 'q'|| c == 'e')
 	continue;
       else
 	putchar(c);
