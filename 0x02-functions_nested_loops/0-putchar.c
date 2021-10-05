d67f49c9bfbf2f322628ab2aae2884d3255ba0ec
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-10-05 04:33:09 -0400

    print _putcahr

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <unistd.h>
+/**
+ * main - prints
+ * Return: 0
+ */
+int main(void)
+
+{
+	printf("_putchar\n");
+	return (0);
+}	
