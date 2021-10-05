b3605d7c0639f352898bf435509434f75204506b
Ghassenehedfi <91055497+Ghassenehedfi@users.noreply.github.com>
2021-10-05 21:16:55 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,6 +16,7 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+void print_times_table(int);
 int add(int, int);
 void print_to_98(int n);
 int main(void);
