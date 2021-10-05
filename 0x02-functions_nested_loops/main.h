14d1b5f6a0df409fbada5699c614089b2fc76f46
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-10-05 05:08:01 -0400

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#include "header.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <unistd.h>
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+int main(void)
+
+{
+puts (test ());
+}
