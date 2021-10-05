a99306811873260d5f80728f0ed9c8202cb561fd
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-10-05 07:48:20 -0400

    fi

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - lowercase
+ * Return: 0
+ *
+ */
+int islower(int c)
+ {
+    int r;
+
+
+    r = islower('H');
+    putchar(r + '0');
+    r = islower('o');
+    putchar(r + '0');
+    r = islower(108);
+    putchar(r + '0');
+    putchar('\n');
+    return (0);
+}
