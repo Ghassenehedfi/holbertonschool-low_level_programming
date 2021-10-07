a3d4292cf8e2a3e44941e13fa1c3e74fa293ef4e
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-10-07 09:08:22 -0400

    f

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,16 +6,20 @@
 void print_diagonal(int n)
 
 {
-        int i, j;
+	int i, j;
 	
 	if (n > 0)
 	for (i = 0; i < n; i++)
 {
-	for (j = 0; j < i + 1; j++)
+	for (j = 0; j < i; j++)
 	{
 	_putchar(' ');
 }
 	_putchar('\\');
 	_putchar('\n');
-	}
+}
+	else
+{
+	_putchar('\n');
+}
 }
