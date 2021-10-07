9875fa057f244a9bcbe7ef2ab999808ec1e3020a
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-10-07 21:40:35 +0100

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - triangle
+ * @size: int
+ */
+	void print_triangle(int size)
+
+{
+
+	int i, j;
+	if (size > 0)
+{
+	for (i = 0; i < size; i++)
+{
+	for (j = 0; j < size - i - 1; j++)
+{
+	_putchar(' ');
+}
+	for (j = 0; j <= i; j++)
+{
+	_putchar('#');
+}
+	_putchar('\n');
+}
+}
+	else
+	_putchar('\n');
+}
