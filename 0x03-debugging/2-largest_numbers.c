7107cfa812e64c443a1514f0d1e1b86259ff0080
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-10-07 21:52:33 +0100

    largest numbers

---
+++ 0x03-debugging/2-largest_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * largest_number - largest numbers
+ * @a: int
+ * @b: int
+ * @c: int
+ * Return: largest number
+ */
+	int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+{
+	largest = a;
+}
+	else if (b >= a && b >= c)
+{
+	largest = b;
+}
+	else
+{
+	largest = c;
+}
+	return (largest);
+}
