dd35f17dbf5e2cbdba5fc1a194a92e3e4780d215
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-10-07 07:38:48 -0400

    g

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,17 +3,15 @@
  * print_most_numbers - don't pring 2 4
  */
 void print_most_numbers(void)
-
 {
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
-	{
-		if (i == '2' || i == '4') 
-			continue;
-		else
+{
+	if (i == '2' || i == '4')
+	continue;
+	else
 		_putchar(i);
 }
 		_putchar('\n');
-
 }
