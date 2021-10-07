64f0d241f091ef6f0f59d47bbc269884e1f69d5f
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-10-07 07:34:23 -0400

    f

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - 0 to 9
+ * @c: int
+ * Return: 0
+ */
+int _isdigit(int c)
+
+{
+	int i;
+
+	for ( i = '0'; i <= '9'; i++)
+{
+	if (i == c)
+	return(1);
+}
+	return (0);
+}
