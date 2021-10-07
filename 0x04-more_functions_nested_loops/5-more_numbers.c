d34f6a3cd50b525385ca2683eb5c34ac2fb648c9
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-10-07 07:48:31 -0400

    f

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,22 +2,22 @@
 /**
  * more_numbers - print numbers
  */
-void more_numbers(void)	
+void more_numbers(void)
 
 {
 
 	int i;
 	int a = 0;
 
-	while (a < 10)
+while (a < 10)
 {
-	for (i = 0; i <= 14; i++)
+for (i = 0; i <= 14; i++)
 {
-	if (i > 9)
+if (i > 9)
 	_putchar('1');
 	_putchar((i % 10) + 48);
 }
-	a++;
+		a++;
 	_putchar('\n');
 }
 }
