8aa68a4476b071998ed248555fabfd75df6e54fd
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-10-07 06:53:09 -0400

    don't pring 2|4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - don't pring 2 4
+ */
+void print_most_numbers(void)
+
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '2' || i == '4') 
+			continue;
+		else
+		_putchar(i);
+}
+		_putchar('\n');
+
+}
