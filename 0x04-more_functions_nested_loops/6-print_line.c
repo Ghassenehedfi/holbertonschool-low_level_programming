ce282890070c94be6aa2fadb7c51626aadbea76f
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-10-07 08:13:09 -0400

    f

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+/**
+ * print_line - straight line
+ * @n: int
+ */
+void print_line(int n)
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+{
+	if (n > 0)
+	_putchar('_');
+}
+	_putchar('\n');
+}
