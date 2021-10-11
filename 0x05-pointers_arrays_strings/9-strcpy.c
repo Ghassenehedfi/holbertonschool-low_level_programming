f2c5c049a1bca50169363b599d7741b41e33af3c
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-10-11 22:58:30 +0100

    double pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - copies an array
+ * @dest: char
+ * @src: char
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+i = 0;
+while (src[i])
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
