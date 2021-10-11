0b6ce7184c564a8cc1b568ed9513e398cbbab926
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-10-11 07:59:45 -0400

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - reverse
+ * @s: char
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+	i++;
+	while (i >= 0)
+	{
+		_putchar(s[i-1]);
+		i--;
+	}
+	_putchar('\n');
+}
