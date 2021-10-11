143a45c89d5fa615df912faf3d8b6b0d71b40037
Ghassenehedfi <https://github.com/Ghassenehedfi>
2021-10-11 06:08:09 -0400

    return the lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - retrun the lenght
+ * @s: char
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0');
+	i++;
+	return (i);
+}
