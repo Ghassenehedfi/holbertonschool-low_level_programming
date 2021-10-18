383cfe20bd072ecbab07743b05e36913b1683026
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-10-18 21:29:57 +0100

    task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * print_chessboard - prints the chessboard
+  * @a: char
+  * Return: 0
+  */
+void print_chessboard(char (*a)[8])
+{
+int b, c;
+
+for (b = 0; b < 8; b++)
+{
+for (c = 0; c < 8; c++)
+{
+_putchar(a[b][c]);
+}
+_putchar('\n');
+}
+}
