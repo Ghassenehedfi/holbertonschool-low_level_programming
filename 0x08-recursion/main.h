7db9d63b8d4a4a28c9804071939615b71a8264d4
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-10-20 01:38:04 -0700

    maing

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+#include <unistd.h>
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int main(void);
+#endif
