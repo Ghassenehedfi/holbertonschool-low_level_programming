3875df64272d4bf057c4b0082c58d5a175b7511d
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-10-20 13:14:23 -0700

    task1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,13 +2,15 @@
 /**
  * _puts_recursion - print string
  * @s: char
+ * Return: 0
  */
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s == '\0')
 {
-	_putchar(*s);
-	s++;
-}
 	_putchar('\n');
+	return;
+}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
