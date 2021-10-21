ca6e2c1a4937f801385f1d3c0509539818a178a8
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-10-21 20:07:24 +0100

    commande

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,13 +7,18 @@
 #include <ctype.h>
 #include <locale.h>
 #include <unistd.h>
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int sqrt_verif(int sq, int n);
 int is_prime_number(int n);
-int _putchar(char c);
-int main(void);
+int prime_checker(int p, int n);
+int is_palindrome(char *s);
+int isPal(char *s, int i, int n);
+int _strlen(char *s);
+int wildcmp(char *s1, char *s2);
 #endif
