f50320de1c9caf6ad973a116716308ef027f7bfc
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-10-22 02:39:51 -0700

    prints it's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints its name
+ * @argc: int
+ * @argv: int
+ * Return: int
+ */
+int main(__attribute__((unused))int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
