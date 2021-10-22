df45faed0b9654ba536bf2dde1da375d9b0b757e
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-10-22 02:24:06 -0700

    commande

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+#include <unistd.h>
+
+int _putchar(char c);
+
+#endif
