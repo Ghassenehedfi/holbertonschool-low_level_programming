0781f8c65643441d9f8422b8de49b3690d10cf73
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-10-22 04:12:25 -0700

    prints all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,8 +5,8 @@
 int main(__attribute__((unused))int argc, __attribute__((unused))char *argv[])
 {
 	int i;
-	
-	for (i = 0;i < argc;i++)
+
+	for (i = 0; i < argc; i++)
 {
 	printf("%s", argv[i]);
 	printf("\n");
