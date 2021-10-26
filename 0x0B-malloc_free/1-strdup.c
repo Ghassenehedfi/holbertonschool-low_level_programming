2e43ed1d5f3314bba91646d70190692e867a814e
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-10-26 22:38:27 +0100

    space

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strdup - allocated space
+ * @str:char
+ * Return: 0
+ */
+	char *_strdup(char *str)
+	{
+	int i = 0, j = 0;
+
+	char *c;
+	if (str == NULL)
+	return (NULL);
+	for (; str[i]; i++)
+	j++;
+	c = malloc((j + 1) * sizeof(char));
+	if (c == 0)
+	return (NULL);
+	for (i = 0; i < j ; i++)
+	c[i] = str[i];
+	c[j] = '\0';
+	return (c);
+}
