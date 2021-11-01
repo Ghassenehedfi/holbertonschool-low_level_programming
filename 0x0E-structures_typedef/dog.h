5e7a2539aec3a0e1a9f51b7b1eb6e858d207060d
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-11-01 22:21:54 +0100

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+}dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
