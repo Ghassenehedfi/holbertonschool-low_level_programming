7a76e40a50870f4cef51d1de329594d3bbdec521
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-11-03 21:52:31 +0100

    searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,13 +11,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 
 	int i = 0;
+
 	if (array != NULL && cmp != NULL)
-{
+	{
 	if (size <= 0)
 	return (-1);
 	else
 	for (; i < size; i++)
-{
+	{
 	if (cmp(array[i]))
 	return (i);
 	else
