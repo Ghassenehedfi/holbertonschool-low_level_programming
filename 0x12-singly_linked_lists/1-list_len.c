37e48dda409f569a501ef739751f24d4b240f53b
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-11-12 14:24:18 -0800

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+nclude "lists.h"
+/**
+ * list_len - printing linked lists
+ * @h: list_t
+ * Return:size_t
+ */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+	size_t len = 0;
+	const list_t *n = h;
+
+	for (; n; i++)
+	{
+		n = n->next;
+		len++;
+	}
+	return (len);
+}
