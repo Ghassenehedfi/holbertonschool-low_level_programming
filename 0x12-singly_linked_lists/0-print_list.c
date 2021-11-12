422e13017415c07353e41c4bfdcbfae838c95ebf
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-11-12 14:12:04 -0800

    print lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * * print_list - linked
+ * * @h: const
+ * * Return: le,
+ * */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+	size_t len = 0;
+	const list_t *n = h;
+	for (; n; i++)
+	{
+		if (n->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", n->len, n->str);
+		n = n->next;
+		len++;
+	}
+	return (len);
+}
