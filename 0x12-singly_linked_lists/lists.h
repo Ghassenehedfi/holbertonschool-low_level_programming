63ad03a31d0d27e62b6bfbeba690249dc6025c74
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-11-12 14:11:14 -0800

    fun

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+#include <unistd.h>
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+
+#endif
