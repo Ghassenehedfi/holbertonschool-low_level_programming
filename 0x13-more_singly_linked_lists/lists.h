bce97fc3ebf1a06d484e0b4988eb3df6b582be3f
Ghassenhedfi <ghassenehedfi@gmail.fr>
2021-11-17 22:54:41 +0100

    header

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,26 +1,22 @@
-fndef _LISTS_H
-#define _LISTS_H
-#include <stdarg.h>
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
-#include <stddef.h>
-#include <string.h>
-
+#include <stdio.h>
 /**
- *  * struct listint_s - singly linked list
- *   * @n: integer
- *    * @next: points to the next node
- *     *
- *      * Description: singly linked list node structure
- *       *
- *        */
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ *
+ */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
-int _putchar(char c);
+/* functions prototype */
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
