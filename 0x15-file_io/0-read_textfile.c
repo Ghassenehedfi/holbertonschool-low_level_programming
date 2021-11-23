d43c66e82279f7d684d4f28f73900d025700c272
Ghassenhedfi <ghassenehedfi@gmail.fr>
2021-11-23 23:52:06 +0100

    file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * ssize_t read_textfile - reads a text file and prints it
+ * *filename: pointer
+ * size_t: size 
+ * Return: int
+ **/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int filename;
+	size_t 
+
+	if (filename == NULL)
+		return(0);
+	{
+		  
