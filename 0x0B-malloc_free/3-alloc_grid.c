64dc642e5e41817189b15e62fcb647b4a7a0d098
Ghassenehedfi <ghassenehedfi@hotmail.fr>
2021-12-07 14:56:20 -0800

    betty

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,18 +1,69 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * alloc_grid - 2 dimensional array of integers
- * @width: int
- * @height: int
- * Return: 0
- */
-	int **alloc_grid(int width, int height)
+ * free_when_f - free j mem of a
+ *
+ * @j: number elements to free
+ * @a: to free from
+ *
+ **/
+void free_when_f(int j, int **a)
 {
-	int i, j, **p;
+	free(a);
+	while (j != -1)
+	{
+		free(a[j]);
+		j--;
+	}
+}
+
+/**
+ * alloc_grid - allocate memory to grid
+ * width of height and initialized with 0
+ *
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: addr of grid
+ **/
+int **alloc_grid(int width, int height)
+{
+	int **a;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
-	return (NULL);
-	p = (int **) malloc(height * sizeof(int *));
-	if (p == NULL)
-	return (NULL);
+	{
+		return (NULL);
+	}
+
+	a = malloc(sizeof(int *) * height);
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	j = 0;
+	while (j < height)
+	{
+		a[j] = malloc(sizeof(int) * width);
+		if (a[j] == NULL)
+		{
+			free_when_f(j - 1, a);
+			return (NULL);
+		}
+		j++;
+	}
+
+
 	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			a[i][j] = 0;
+		}
+
+	}
+	return (a);
 }
